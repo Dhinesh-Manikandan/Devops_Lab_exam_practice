a67313055d0e875406962ea54cf5d3527d1740ad
Dhinesh Manikandan <dhineshsidhu@gmail.com>
2026-09-23 09:10:00 +0530

    From feature branch

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -18,6 +18,6 @@ public class StudentGradeCalculator {
     public static void main(String[] args) {
         StudentGradeCalculator calculator = new StudentGradeCalculator();
 
-        System.out.println("Grade: " + calculator.calculateGrade(85));
+        System.out.println("Grade: " + calculator.calculateGrade(95));
     }
 }
